102271c929f4aa80118661feba7037d853d497e3
biswajitmailid <biswajitmailid@gmail.com>
2020-11-19 20:25:36 +0000

    Read Transactions from xml.

---
+++ ApplicationService.UnitTests/TransactionReaders/XmlTransactionReaderTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using ApplicationCore.Interfaces.Services;
+using ApplicationService.TransactionReaders;
+using Xunit;
+
+namespace ApplicationService.UnitTests.TransactionReaders
+{
+    public class XmlTransactionReaderTest
+    {
+        private readonly ITransactionFileReader _transactionFileReader;
+        public XmlTransactionReaderTest()
+        {
+            _transactionFileReader = new XmlTransactionReader();
+        }
+
+        [Fact]
+        public async Task Read_data_from_valid_file()
+        {
+            // arrange and act
+            var transactions = await _transactionFileReader.Read("./Setup/2c2p.xml");
+
+            //assert
+            Assert.Equal(2, transactions.Count);
+        }
+    }
+}
---
+++ ApplicationService/TransactionReaders/XmlTransactionReader.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using ApplicationCore.Interfaces.Services;
+using ApplicationCore.Models;
+
+namespace ApplicationService.TransactionReaders
+{
+    public class XmlTransactionReader : ITransactionFileReader
+    {
+        public string FileType => "xml";
+
+        public async Task<List<Transaction>> Read(string path)
+        {
+            var xmlFileContent = await GetXmlFileContent(path);
+            var xmlTransactions = DeserializeXmlContent(xmlFileContent);
+            return CopyToTransactionList(xmlTransactions);
+        }
+
+        #region Supported Methods
+
+        private Task<string> GetXmlFileContent(string filePath)
+        {
+            return File.ReadAllTextAsync(filePath);
+        }
+
+        private XmlTransactions DeserializeXmlContent(string xmlContent)
+        {
+            var serializer = new XmlSerializer(typeof(XmlTransactions));
+            using TextReader reader = new StringReader(xmlContent);
+            return (XmlTransactions) serializer.Deserialize(reader);
+        }
+
+        private List<Transaction> CopyToTransactionList(XmlTransactions xmlTransactions)
+        {
+            var transactions = xmlTransactions.XmlTransactionModel.Select(x => new Transaction
+            {
+                TransactionId = x.TransactionId,
+                TransactionDate = x.TransactionDate,
+                Amount = x.PaymentDetails.Amount,
+                CurrencyCode = x.PaymentDetails.CurrencyCode,
+                Status = x.Status
+            }).ToList();
+            return transactions;
+        }
+
+        #endregion
+    }
+
+    [XmlRoot("Transactions")]
+    public class XmlTransactions
+    {
+        [XmlElement("Transaction")] public List<XmlTransactionModel> XmlTransactionModel { get; set; }
+    }
+
+    public class XmlTransactionModel
+    {
+        [XmlAttribute("id")] public string TransactionId { get; set; }
+
+        [XmlElement("TransactionDate")] public DateTime TransactionDate { get; set; }
+
+        [XmlElement("PaymentDetails")] public PaymentDetails PaymentDetails { get; set; }
+
+        [XmlElement("Status")] public string Status { get; set; }
+    }
+
+    public class PaymentDetails
+    {
+        [XmlElement("Amount")] public decimal Amount { get; set; }
+
+        [XmlElement("CurrencyCode")] public string CurrencyCode { get; set; }
+    }
+}
